4c45fab525a20cef50fe34fd548cddae1b561d3b
Pratheesh <Pratheesh.SATHYANARAYNAN.Contractor@3ds.com>
2023-06-16 12:37:16 +0530

    update with components

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import AzureADProvider from "next-auth/providers/azure-ad"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export const authOptions: NextAuthOptions = {
+  // https://next-auth.js.org/configuration/providers/oauth
+  providers: [
+
+    AzureADProvider({
+           clientId: process.env.AZURE_AD_CLIENT_ID,
+           clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+           tenantId: process.env.AZURE_AD_TENANT_ID,
+         }),
+  ],
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin"
+      return token
+    },
+  },
+  debug:true
+}
+
+export default NextAuth(authOptions)
