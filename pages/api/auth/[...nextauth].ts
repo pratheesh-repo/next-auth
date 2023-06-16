4c625516fc8ff243260faadfb5f44f428a7f02df
Pratheesh <Pratheesh.SATHYANARAYNAN.Contractor@3ds.com>
2023-06-16 13:52:07 +0530

    add provider info

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,9 +8,9 @@ export const authOptions: NextAuthOptions = {
   providers: [
 
     AzureADProvider({
-           clientId: process.env.AZURE_AD_CLIENT_ID || "1",
-           clientSecret: process.env.AZURE_AD_CLIENT_SECRET || "2",
-           tenantId: process.env.AZURE_AD_TENANT_ID || "3",
+           clientId: process.env.AZURE_AD_CLIENT_ID || "3ba44ae1-831d-4943-8abc-7ba35bfb18c7",
+           clientSecret: process.env.AZURE_AD_CLIENT_SECRET || "Wd88Q~5AzjWZkvr64MDPDjLBUcPcscrNL8B5Xa8m",
+           tenantId: process.env.AZURE_AD_TENANT_ID || "08eea03a-f2ff-4528-af12-25d864760ffe",
          }),
   ],
   callbacks: {
